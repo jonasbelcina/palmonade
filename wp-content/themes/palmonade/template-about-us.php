41a2e5decc1d7c26ae57103b53755f723a793ece
Jonas Belcina <jonasbelcina10@gmail.com>
2016-02-28 11:43:25 +0400

    Create About Us template

--- wp-content/themes/palmonade/template-about-us.php
+++ wp-content/themes/palmonade/template-about-us.php
@@ -7,6 +7,31 @@
 
 get_header(); ?>
 
+<section class="company about-us">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<?php if ( have_posts() ) :
+					while ( have_posts() ) : the_post();
+						the_content();
+					endwhile;
+				endif; ?>
+			</div>
 
+			<div class="col-md-6">
+				<h2><?php the_field('director_heading'); ?></h2>
+				<?php $director_img = get_field('director_image'); ?>
+				<div class="director-img">
+					<img class="img-responsive" src="<?php echo $director_image['url']; ?>" alt="<?php echo $director_image['alt']; ?>">
+					<span><?php the_field('director_position_name'); ?></span>
+				</div>
+
+				<div class="director-msg">
+					<?php the_field('director_message'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
