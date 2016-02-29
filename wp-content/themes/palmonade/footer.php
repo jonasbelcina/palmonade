e829a4f06a458210519cd57a8514df6223b82664
Jonas Belcina <jonasbelcina10@gmail.com>
2016-02-29 17:32:56 +0400

    Add id for footer newsletter

--- wp-content/themes/palmonade/footer.php
+++ wp-content/themes/palmonade/footer.php
@@ -51,12 +51,8 @@
 						<?php wp_nav_menu( array( 'theme_location' => 'quick-links' ) ); ?>
 					</div>
 
-					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 footer-col">
+					<div id="footer-newsletter" class="col-lg-3 col-md-3 col-sm-6 col-xs-6 footer-col">
 						<h2 class="footer-drop">Newsletter <span class="glyphicon glyphicon-menu-down"></span></h2>
-						<!-- <form>
-							<input type="email" placeholder="Enter your Email Address"></input>
-							<input type="submit" value="Join"></input>
-						</form> -->
 						<?php echo do_shortcode('[mc4wp_form id="124"]'); ?>
 
 						<div class="footer-social">
