03c6db16b5b20698d7b2377e4d4e96b5ed74a7fa
Jonas Belcina <jonasbelcina10@gmail.com>
2016-03-01 10:02:23 +0400

    Fix favicon

--- wp-content/themes/palmonade/header.php
+++ wp-content/themes/palmonade/header.php
@@ -17,8 +17,8 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="shortcut icon" href="<?php the_field('favicon' , 'options'); ?>" />
 	<?php endif; ?>
+	<link rel="shortcut icon" href="<?php the_field('favicon' , 'options'); ?>" />
 	<?php wp_head(); ?>
 </head>
 
